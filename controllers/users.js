d70d5532f4c2ced64f83cdf2c4fee5ad4f4fdf0d
sv-vin <sveta.vinahradava@yandex.by>
2022-04-03 23:22:47 +0300

    k

--- controllers/users.js
+++ controllers/users.js
@@ -2,7 +2,13 @@ const User = require('../models/user');
 
 module.exports.getUsers = (req, res) => {
   User.find({})
-    .then((users) => res.send({ data: users }))
+    .then((users) => {
+      if (!users) {
+        res.status(404).send({ message: 'Пользователь не найден' });
+      } else {
+        res.send({ data: users });
+      }
+    })
     .catch(() => res.status(500).send({ message: 'Произошла ошибочка' }));
 };
 
