43ffcc01386ccada1f9af2283c6cb8045443ea21
sv-vin <sveta.vinahradava@yandex.by>
2022-04-07 13:46:31 +0300

    M

--- app.js
+++ app.js
@@ -1,11 +1,10 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
-const routerUser = require('./routes/users');
-const routerCard = require('./routes/cards');
+const usersRouter = require('./routes/users');
+const cardsRouter = require('./routes/cards');
 
 const { PORT = 3000 } = process.env;
-
 const app = express();
 
 app.use(bodyParser.json());
@@ -15,15 +14,15 @@ mongoose.connect('mongodb://localhost:27017/mestodb ', {
   useNewUrlParser: true,
 });
 
-app.use('/users', routerUser);
-app.use('/cards', routerCard);
-
 app.use((req, res, next) => {
   req.user = {
     _id: '6248741a5e2ca84661c0bdd5',
   };
   next();
 });
+
+app.use('/users', usersRouter);
+app.use('/cards', cardsRouter);
 
 app.use((req, res) => {
   res.status(404).send({ message: 'Не корректный URL' });
