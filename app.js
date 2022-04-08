867f11dfbc28701e458b932565ac8e340ab81097
sv-vin <sveta.vinahradava@yandex.by>
2022-04-08 15:21:55 +0300

    main

--- app.js
+++ app.js
@@ -1,14 +1,17 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
+// const bodyParser = require('body-parser');
 const usersRouter = require('./routes/users');
 const cardsRouter = require('./routes/cards');
 
 const { PORT = 3000 } = process.env;
 const app = express();
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+// app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 mongoose.connect('mongodb://localhost:27017/mestodb ', {
   useNewUrlParser: true,
@@ -28,4 +31,4 @@ app.use((req, res) => {
   res.status(404).send({ message: 'Не корректный URL' });
 });
 
-app.listen(PORT, () => console.log(`Сервер запущен на порту ${PORT}`));
+app.listen(PORT);
